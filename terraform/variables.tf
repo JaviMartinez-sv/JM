521c5610ef8ec41c33e3d6b1a0bccd57fac24da1
JaviMartinez-sv <73138555+JaviMartinez-sv@users.noreply.github.com>
2021-02-24 15:59:48 +0100

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,11 @@
 variable "resource_group" {
   description = "The resource group"
-  default = "CHANGE_ME_RESOURCE_GROUP"
+  default = "JM_RESOURCE_GROUP"
 }
 
 variable "application_name" {
   description = "The Spring Boot application name"
-  default     = "CHANGE_ME_APP_NAME"
+  default     = "JM_APP_NAME"
 }
 
 variable "location" {
